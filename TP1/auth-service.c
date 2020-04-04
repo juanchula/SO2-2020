a951ee4ee4975e34c5d4e5fa99873b4b3f6388f2
Juan Ignacio Fernandez <jruloignacio@hotmail.com>
2020-04-04 18:32:58 -0300

    Se agrego metodo para cambiar linea de archivo, loguearse,cambiar contraseña. Falta metodo para mostrar datos y que al loguearse modifique la fecha del ultimo ingreso

--- TP1/auth-service.c
+++ TP1/auth-service.c
@@ -12,26 +12,63 @@
 //     free(final);
 //     return strdup(final);
 // }
+void changeline(int line, char *txt){
+    int i = 0;
+    char temp[200];
+    FILE *original = fopen("/home/juanfernandez/Facu/SO2-2020/TP1/base-datos.txt", "r");
+    FILE *new =  fopen("/home/juanfernandez/Facu/SO2-2020/TP1/base-datos2.txt", "w");
+    if(original == NULL || new == NULL){
+        perror("No se a podido abrir el archivo: ");
+        exit(EXIT_FAILURE);
+    }
+
+    while (fgets(temp, 200, original)){
+        if(i==line){
+            fputs(txt, new);
+        }else{
+            fputs(temp, new);
+        }
+        i++;
+    }
+    fclose(original);
+    fclose(new);
+
+    remove("/home/juanfernandez/Facu/SO2-2020/TP1/base-datos.txt");
+    rename("/home/juanfernandez/Facu/SO2-2020/TP1/base-datos2.txt", "/home/juanfernandez/Facu/SO2-2020/TP1/base-datos.txt");
+    
+}
 
 int trylogin(char *user, char * userpassword){
+    char txt[200];
+    int line = 0;
+    char edit[200] = "";
+
     FILE *database = fopen("/home/juanfernandez/Facu/SO2-2020/TP1/base-datos.txt", "r");
     if(database == NULL){
         perror("No se a podido abrir el archivo: ");
         exit(EXIT_FAILURE);
     }
-    char txt[200];
-    int line = 0;
 
     while(fgets(txt, 200, database)){
         if(strstr(txt, user) != NULL){
             if(strstr(txt, userpassword) != NULL && strstr(txt, "$$$") != NULL){
                 //eliminar los $
                 fclose(database);
+                strcat(edit, userpassword);
+                strcat(edit, "\n");
+                changeline(line, userpassword);
                 return line;
             }else{
-                fclose(database);
-                //agregar un $
-                return -1;
+                if(strstr(txt, "$$$") == NULL){
+                    strcat(edit, txt);
+                    strcat(edit, "$");
+                    strcat(edit, "\n");
+                    fclose(database);
+                    changeline(line, edit);
+                    return -1;
+                    //agregar $
+                }
+                break;
             }
         }
         line++;
@@ -56,11 +93,23 @@ bool login(char *user, char * password){
 // sscanf(line, "%*s %s %*s %*s %*s %s", r, w);
 }
 
+void changepassword( int line, char *user, char *newpassword){
+    char userpassword[200] = "<";
+    strcat(userpassword, user);
+    strcat(userpassword, "><<");
+    strcat(userpassword, newpassword);
+    strcat(userpassword, ">>");
+    changeline(line, userpassword);
+}
+
 
 int main()
 {
-    char * hola = "Hola";
-    hola = convert(hola);
-    printf("%s", hola);
+    // char * lastuser;
+    // char * lastpassword;
+    // int line;
+    // char * hola = "Hola";
+    // hola = convert(hola);
+    // printf("%s", hola);
     return 0;
 }
--- TP1/pruebitas.c
+++ TP1/pruebitas.c
@@ -3,8 +3,38 @@
 #include <stdlib.h>
 
 
+
 int main()
 {
+    int line = 5;
+    char *txt = "ESTA NUEVA LINEA DEBE ESTAR EN 6\n";
+    int i = 0;
+    char temp[200];
+    FILE *original = fopen("/home/juanfernandez/Facu/SO2-2020/TP1/a.txt", "r");
+    FILE *new =  fopen("/home/juanfernandez/Facu/SO2-2020/TP1/b.txt", "w");
+    if(original == NULL || new == NULL){
+        perror("No se a podido abrir el archivo: ");
+        exit(EXIT_FAILURE);
+    }
+
+    while (fgets(temp, 200, original)){
+        if(i==line){
+            fputs(txt, new);
+        }else{
+            fputs(temp, new);
+        }
+        i++;
+    }
+    fclose(original);
+    fclose(new);
+
+    remove("/home/juanfernandez/Facu/SO2-2020/TP1/a.txt");
+    rename("/home/juanfernandez/Facu/SO2-2020/TP1/b.txt", "/home/juanfernandez/Facu/SO2-2020/TP1/a.txt");
+    
+
+
+
+
     //char *a = "Hola capo, todo bien\n Se, todo bien.";
     // do{
 
@@ -12,14 +42,16 @@ int main()
     //     printf("%s", a);
     //     bzero(a, 20); 
     // }while(1);
-    FILE *database = fopen("/home/juanfernandez/Facu/SO2-2020/TP1/base-datos.txt", "r");
-    char txt[200];
-    int line = 0;
+    // FILE *database = fopen("/home/juanfernandez/Facu/SO2-2020/TP1/base-datos.txt", "r");
+    // char txt[200];
+    // int line = 0;
 
-    while(fgets(txt, 200, database)){
-        printf("%s", database);
-        //printf("%s linea:%i",txt,line);
-        line++;
-    }
+    // while(fgets(txt, 200, database)){
+    //     printf("%s linea:%i",txt,line);
+    //     line++;
+    // }
+
+
+    
     return 0;
 }
