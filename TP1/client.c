d4fb479c90242f6bbf9155005d8bfa0d1207d81a
Juan Ignacio Fernandez <jruloignacio@hotmail.com>
2020-04-08 02:49:37 -0300

    prueba

--- TP1/client.c
+++ TP1/client.c
@@ -5,15 +5,20 @@
 #include <netinet/in.h>
 #include <unistd.h>
 #include <string.h>
+#include <stdbool.h>
 
 #define MAX 200 
-#define PORT 4444
+#define PORTMSJ 4444
+#define PORTFILE 5555
+
 
 int main() 
 {
+    bool transfer = false;
     char msjserver[MAX]; 
     char msjclient[MAX];  
-    int sockfd; 
+    int sockfd;
+    int sockfile; 
   
     // socket create and varification 
     sockfd = socket(AF_INET, SOCK_STREAM, 0); 
@@ -23,26 +28,56 @@ int main()
     }
   
     // create and assign IP, PORT 
-    struct sockaddr_in * servaddr = calloc(1, sizeof (struct sockaddr_in));
-    servaddr->sin_family = AF_INET;
-    servaddr->sin_port = htons(PORT);
-    servaddr->sin_addr.s_addr = INADDR_ANY;
+    struct sockaddr_in * servaddrmsj = calloc(1, sizeof (struct sockaddr_in));
+    servaddrmsj->sin_family = AF_INET;
+    servaddrmsj->sin_port = htons(PORTMSJ);
+    servaddrmsj->sin_addr.s_addr = INADDR_ANY;
+
+
+
+    sockfile = socket(AF_INET, SOCK_STREAM, 0); 
+    if (sockfile == -1) { 
+        perror("socket creation failed...\n"); 
+        exit(EXIT_FAILURE); 
+    }
+  
+    // create and assign IP, PORT 
+    struct sockaddr_in * servaddrfile = calloc(1, sizeof (struct sockaddr_in));
+    servaddrfile->sin_family = AF_INET;
+    servaddrfile->sin_port = htons(PORTFILE);
+    servaddrfile->sin_addr.s_addr = INADDR_ANY;
   
     // connect the client socket to server socket 
-    if (connect(sockfd, (struct sockaddr *)  servaddr, sizeof (struct sockaddr)) != 0) { 
+    if (connect(sockfd, (struct sockaddr *)  servaddrmsj, sizeof (struct sockaddr)) != 0) { 
         perror("connection with the server failed...\n"); 
         exit(EXIT_FAILURE);
     } 
-  
-    do{
-        fgets( msjserver, MAX-1, stdin );
-        send(sockfd, msjserver, MAX, 0);
-        bzero(msjserver, MAX); 
+    while(1){
+        do{
+            fgets( msjserver, MAX-1, stdin );
+            send(sockfd, msjserver, MAX, 0);
+            bzero(msjserver, MAX); 
 
-        recv(sockfd,msjclient, MAX, 0);
-        printf("%s\n", msjclient);
-        bzero(msjclient, sizeof(msjclient)); 
-    }while(1); 
+            recv(sockfd,msjclient, MAX, 0);
+            printf("%s", msjclient);
+            if(strstr(msjserver, "Conectar puerto de transferencia.")){
+                transfer = true;
+            }
+            bzero(msjclient, sizeof(msjclient)); 
+        }while(!transfer);
+        if (connect(sockfile, (struct sockaddr *)  servaddrfile, sizeof (struct sockaddr)) != 0) { 
+            perror("connection with the server failed...\n"); 
+            exit(EXIT_FAILURE);
+        }
+        bzero(msjclient, MAX);
+        int recibido = -1;
+        FILE *image = fopen("./isoscopia/nada", "wb");
+        while((recibido = recv(sockfile, msjserver, MAX, 0)) > 0){
+            printf("%s",msjserver);
+            fwrite(msjserver,sizeof(char),1,image);
+        }
+        fclose(image);
+        transfer = false; 
   
     close(sockfd); 
 } 
--- TP1/file_service.c
+++ TP1/file_service.c
@@ -5,10 +5,13 @@
 #include <stdbool.h>
 #include <openssl/md5.h>
 #include <mqueue.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 
 #define QUEUEPATH "/file_service"
 #define BUFF_SIZE 200
 #define DIRECTORY "./isos/"
+#define PORT 5555
 
 int amountspace(char * txt){
     int space = 0;
@@ -120,12 +123,86 @@ void lsfile(char *txt){
     }
 }
 
+bool fileverification(char *filename, char *txt){
+    bzero(txt, BUFF_SIZE);
+    int long s;
+    char size[BUFF_SIZE] = "";
+    char iso[BUFF_SIZE] = "";
+    char md5[BUFF_SIZE] = "";
+    char iso[BUFF_SIZE] = "";
+    DIR *d = opendir("./isos");
+    struct dirent *dentry;
+    
+    if(d != NULL){
+        while((dentry=readdir(d)) != NULL){
+            strcpy(iso, dentry->d_name);
+            if(comparetxt((int) strlen(filename), iso, filename)){
+                if(strstr(iso, ".iso") !=NULL){
+                calcmd5(iso, md5);
+                s = filesize(iso);
+                sprintf(size, "%ld", s);
+                strcat(txt, iso);
+                strcat(txt, "   ");
+                strcat(txt, size);
+                strcat(txt, "B   ");
+                strcat(txt, md5);
+                // strcat(txt, "\n");
+                return true;
+                }
+            }
+        }
+        closedir(d);
+    }else{
+        perror("No se ha podido abrir el directorio: ");
+        exit (EXIT_FAILURE);
+    }
+    return false;
+}
+
 int main(){
+    bool transfer = false;
     int space;
     char argone[BUFF_SIZE] = "";
     char argtwo[BUFF_SIZE] = "";
+    char argthree[BUFF_SIZE] = "";
     char sent_msg[BUFF_SIZE] = "";
     char recv_msg[BUFF_SIZE] = "";
+    char filetransfer[BUFF_SIZE] = "";
+    char url[BUFF_SIZE] = "";
+
+
+    char msjserver[BUFF_SIZE]; 
+    char msjclient[BUFF_SIZE]; 
+    int sfd;
+    int fdc;
+    bool connected = false;
+    bool login = false;
+
+    sfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sfd == -1) { 
+        perror("socket creation failed...\n"); 
+        exit(EXIT_FAILURE);
+    }
+
+    struct sockaddr_in* server = calloc(1, sizeof (struct sockaddr_in));
+    server->sin_family = AF_INET;
+    server->sin_port = htons(PORT);
+    server->sin_addr.s_addr = INADDR_ANY;
+
+    struct sockaddr_in* client = calloc(1, sizeof (struct sockaddr_in));
+    int32_t lenght_client = (int32_t) sizeof (struct sockaddr_in);
+
+    if(bind(sfd, (struct sockaddr *) server, sizeof (struct sockaddr)) == -1){
+        perror("Error en bind: ");
+        exit(EXIT_FAILURE);
+    }
+    
+    if(listen(sfd, 1024) == -1){
+        perror("Error en listen: ");
+        exit(EXIT_FAILURE);
+    }
+
+
 
     mqd_t qd = mq_open(QUEUEPATH, O_RDWR);
     if (qd == -1){
@@ -133,34 +210,78 @@ int main(){
             exit(EXIT_FAILURE);
     }
     unsigned int prio = 1;
+    while(1){
+        do{
+            bzero(recv_msg, BUFF_SIZE);
+            bzero(sent_msg, BUFF_SIZE);
+            if (mq_receive(qd, recv_msg, BUFF_SIZE, &prio) == -1 ){
+                        perror("Error al recibir mensaje en el servicio de autentificacion: ");
+                        exit(EXIT_FAILURE);
+                }
 
-    do{
-        bzero(recv_msg, BUFF_SIZE);
+            strtok(recv_msg, "\n");
+            space = amountspace(recv_msg);
+            switch (space){
+            case 1: sscanf(recv_msg, "%s %s", argone, argtwo);
+                    if(comparetxt(4, argone, "file") && comparetxt(2, argtwo, "ls")){
+                        lsfile(sent_msg);
+                    } else{
+                        strcpy(sent_msg, "Comando incorrecto");
+                    }
+                    bzero(argone, BUFF_SIZE);
+                    bzero(argtwo, BUFF_SIZE);
+                    break;
+
+            case 2: sscanf(recv_msg, "%s %s %s", argone, argtwo, argthree);
+                    if(comparetxt(4, argone, "file") && comparetxt(4, argtwo, "down") && fileverification(argthree, filetransfer)){
+                        transfer = true;
+                        bzero(argone, BUFF_SIZE);
+                        bzero(argtwo, BUFF_SIZE);
+                        strcpy(sent_msg, "Conectar puerto de transferencia. Datos de iso: ");
+                        strcat(sent_msg, filetransfer);
+                    } else{
+                        strcpy(sent_msg, "Comando incorrecto");
+                        bzero(argone, BUFF_SIZE);
+                        bzero(argtwo, BUFF_SIZE);
+                        bzero(argthree, BUFF_SIZE);
+                    }
+                break;
+            
+            default: strcpy(sent_msg, "Comando incorrecto");
+                break;
+            }
+
+            if (  mq_send(qd, sent_msg, BUFF_SIZE, (unsigned int) 1) == -1){
+                perror("Error en envio de mensaje de servicio de autentificacion: ");
+                exit(EXIT_FAILURE);
+            }
+        }while(!transfer);
+        fdc = accept(sfd, (struct sockaddr *) client, (socklen_t *) &lenght_client);
+        lenght_client = (int32_t) sizeof (struct sockaddr_in);
+        bzero(url, BUFF_SIZE);
+        strcpy(url, DIRECTORY);
+        strcat(url, argthree);
+
+        FILE *image = fopen(url, "rb");
+        if (image == NULL) {
+            perror("No se ha podido abrir la isos: ");
+            exit (EXIT_FAILURE);
+        }
+
+        while(!feof(image)){
+            fread(sent_msg,sizeof(char),BUFF_SIZE,image);
+            if(send(fdc,sent_msg,BUFF_SIZE,0) == -1){
+                perror("Error al enviar el arvhivo:");
+            }
+        }
+
+        bzero(argthree, BUFF_SIZE);
+        bzero(filetransfer, BUFF_SIZE);
         bzero(sent_msg, BUFF_SIZE);
-        if (mq_receive(qd, recv_msg, BUFF_SIZE, &prio) == -1 ){
-                    perror("Error al recibir mensaje en el servicio de autentificacion: ");
-                    exit(EXIT_FAILURE);
-            }
+        transfer = false;
+    }
 
-        strtok(recv_msg, "\n");
-        space = amountspace(recv_msg);
-        if(space == 1){
-            sscanf(recv_msg, "%s %s", argone, argtwo);
-            if(comparetxt(4, argone, "file") && comparetxt(2, argtwo, "ls")){
-                lsfile(sent_msg);
-            } else{
-                strcpy(sent_msg, "Comando incorrecto");
-            }
-            bzero(argone, BUFF_SIZE);
-            bzero(argtwo, BUFF_SIZE);
-        }else{
-            strcpy(sent_msg, "Comando incorrecto");
-        }
-        if (  mq_send(qd, sent_msg, BUFF_SIZE, (unsigned int) 1) == -1){
-            perror("Error en envio de mensaje de servicio de autentificacion: ");
-            exit(EXIT_FAILURE);
-        }
-    }while(1);
+
 
     char txt[BUFF_SIZE] = "";
     //char file[BUFF_SIZE] = "generadorkey2";
